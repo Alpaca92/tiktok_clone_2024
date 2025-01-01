7746692708aff5c2fdaba7ad89bb36c728a9a9d1
ayaan <zxcvbnm5288@gmail.com>
2025-01-02 08:57:57 +0900

    feat: init commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFE9435A)),
+          useMaterial3: true,
+        ),
+        home: Container());
+  }
+}
